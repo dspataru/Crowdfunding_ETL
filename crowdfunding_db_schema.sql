894cef3cf57b1a9db929dbcfb131f713e0922b5f
dspataru <61765352+dspataru@users.noreply.github.com>
2023-09-02 11:49:52 -0400

    updating crowdfunding db files

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,63 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- ERD for Project 2 ETL
+-- Course: UTOR-VIRT-DATA-PT-06-2023-U-LOLC
+-- Authors: Daiana Spataru, Jasmine Bamba
+-- Context: Create ERC for the Crowdfunding Database
+
+CREATE TABLE "Contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" string   NOT NULL,
+    "last_name" string   NOT NULL,
+    "email" string   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "Campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company" string   NOT NULL,
+    "blurb" string   NOT NULL,
+    "goal" int   NOT NULL,
+    "pledged" int   NOT NULL,
+    "outcome" string   NOT NULL,
+    "backers" int   NOT NULL,
+    "country" string   NOT NULL,
+    "currency" string   NOT NULL,
+    "launched_at" date   NOT NULL,
+    "deadline" date   NOT NULL,
+    "category_id" int   NOT NULL,
+    "subcategory_id" int   NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" int   NOT NULL,
+    "subcategory" int   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "Category" (
+    "category_id" int   NOT NULL,
+    "category" int   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Contacts" ("contact_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "Category" ("category_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategory" ("subcategory_id");
+
---
+++ tables_creation.sql
@@ -0,0 +1,56 @@
+-- Creating a table for the contact information of each campaign representative
+CREATE TABLE contacts (
+	contact_id INT NOT NULL,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(100),
+	PRIMARY KEY (contact_id)
+)
+
+SELECT *
+FROM contacts
+
+-- Creating a table for the different category of each campaign
+CREATE TABLE category (
+	category_id INT NOT NULL,
+	category VARCHAR(50) NOT NULL,
+	PRIMARY KEY (category_id)
+);
+
+SELECT *
+FROM category;
+
+-- Creating a table for the different subcategory of each campaign
+CREATE TABLE subcategory (
+	subcategory_id INT NOT NULL,
+	subcategory VARCHAR(50) NOT NULL,
+	PRIMARY KEY (subcategory_id)
+);
+
+SELECT *
+FROM subcategory;
+
+-- Creating a table for the information about each registered campaign
+CREATE TABLE campaign (
+	cf_id INT NOT NULL,
+	contact_id INT NOT NULL,
+	company VARCHAR(250) NOT NULL,
+	blurb VARCHAR,
+	goal INT NOT NULL,
+	pledged INT NOT NULL,
+	outcome VARCHAR(10) NOT NULL,
+	backers INT NOT NULL,
+	country VARCHAR(2),
+	currency VARCHAR(3),
+	launched_at FLOAT,
+	deadline FLOAT,
+	category_id INT NOT NULL,
+	subcategory_id INT NOT NULL,
+	PRIMARY KEY (cf_id),
+	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+	FOREIGN KEY (category_id) REFERENCES category(category_id),
+	FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
+
+SELECT *
+FROM campaign;
